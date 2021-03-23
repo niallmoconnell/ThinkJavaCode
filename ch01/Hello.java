547874844676e8249c29b785501db728395292be
noconnell <noconnell@deloitte.ie>
2021-03-23 18:43:31 +0000

    Chapter 1 Exercises Complete

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-        // generate some simple output
+    // generate some simple output
         System.out.println("Hello, World!");
-    }
+    //Wait, what!?
+        System.out.println("How are you?");
 
+    }
 }
